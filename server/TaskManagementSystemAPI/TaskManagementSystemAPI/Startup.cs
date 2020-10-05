7fa8a4bebd65b387aaae1800cfedc67d8fcb9686
Elsevar Rajabli <eradjabli7@gmail.com>
2020-10-05 20:01:00 +0300

    Update Startup.cs

--- server/TaskManagementSystemAPI/TaskManagementSystemAPI/Startup.cs
+++ server/TaskManagementSystemAPI/TaskManagementSystemAPI/Startup.cs
@@ -66,23 +66,22 @@ namespace TaskManagementSystemAPI
                 });
 
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-      {
-        {
-          new OpenApiSecurityScheme
-          {
-            Reference = new OpenApiReference
-              {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-              },
-              Scheme = "oauth2",
-              Name = "Bearer",
-              In = ParameterLocation.Header,
-
-            },
-            new List<string>()
-          }
-        });
+                {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+                      },
+                        new List<string>()
+                    }
+                });
             });
         }
 
