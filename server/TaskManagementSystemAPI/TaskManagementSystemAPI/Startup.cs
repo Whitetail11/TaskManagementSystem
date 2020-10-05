7d85348eb5dcb185b64c758ab12e2c8eb977613f
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-10-05 19:16:38 +0300

    create authorize button

--- server/TaskManagementSystemAPI/TaskManagementSystemAPI/Startup.cs
+++ server/TaskManagementSystemAPI/TaskManagementSystemAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TaskManagementSystemAPI.Classes;
 using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
 
 namespace TaskManagementSystemAPI
 {
@@ -50,9 +51,38 @@ namespace TaskManagementSystemAPI
 
             services.AddControllers();
 
-            services.AddSwaggerGen(options =>
+            services.AddSwaggerGen(c =>
             {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Task Management System", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Task Management System", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+          }
+        });
             });
         }
 
@@ -71,6 +101,8 @@ namespace TaskManagementSystemAPI
             app.UseAuthorization();
 
             app.UseSwagger();
+
+
 
             app.UseSwaggerUI(c =>
             {
