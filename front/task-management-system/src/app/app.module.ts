767961e926e24e17d339f11ef07693535667a8d2
Elsevar Rajabli <eradjabli7@gmail.com>
2020-10-10 22:32:38 +0300

    added navbar, login and register components

--- front/task-management-system/src/app/app.module.ts
+++ front/task-management-system/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
