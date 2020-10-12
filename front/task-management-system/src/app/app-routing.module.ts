f9265a0590fc573a62f9960abc655d225d95870d
Elsevar Rajabli <eradjabli7@gmail.com>
2020-10-12 13:40:16 +0300

    renamed register component to signup

--- front/task-management-system/src/app/app-routing.module.ts
+++ front/task-management-system/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { SignupComponent } from './signup/signup.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'tasks', component: TasksComponent }
 ];
 
--- front/task-management-system/src/app/app.module.ts
+++ front/task-management-system/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { ACCESS_TOKEN_KEY } from './services/account.service';
 import { JwtModule } from '@auth0/angular-jwt'
 import { FormsModule} from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { TasksComponent } from './tasks/tasks.component';
+import { SignupComponent } from './signup/signup.component';
  
 export function tokenGetter() {
   return localStorage.getItem(ACCESS_TOKEN_KEY);
@@ -21,8 +21,8 @@ export function tokenGetter() {
     AppComponent,
     NavMenuComponent,
     LoginComponent,
-    RegisterComponent,
-    TasksComponent
+    TasksComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
