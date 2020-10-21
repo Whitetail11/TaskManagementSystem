137995b8c6d08b5a62cf6286263450ca7456ce76
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-10-21 22:22:49 +0300

    Update TaskController.cs

--- server/TaskManagementSystemAPI/TaskManagementSystemAPI/Controllers/TaskController.cs
+++ server/TaskManagementSystemAPI/TaskManagementSystemAPI/Controllers/TaskController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.DTOs;
 using BusinessLayer.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,18 +20,21 @@ namespace TaskManagementSystemAPI.Controllers
             this._tasksService = _tasksService;
         }
         [HttpGet]
+        [Authorize]
         public IActionResult GetTasks()
         {
             List<TaskDTO> objectList = _tasksService.GetTasks();
             return Ok(objectList);
         }
         [HttpPost]
+        [Authorize]
         public IActionResult CreateTask(TaskDTO task, string email)
         {
             _tasksService.CreateTask(task, email);
             return Ok();
         }
         [HttpDelete]
+        [Authorize]
         public IActionResult Delete(int id)
          {
             _tasksService.Delete(id);
