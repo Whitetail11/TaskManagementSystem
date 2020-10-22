20beae19ac54f5efd92f8c1bca69f0597f642545
Elsevar Rajabli <eradjabli7@gmail.com>
2020-10-22 17:47:04 +0300

    added service collection extension to add application dependencies

---
+++ server/TaskManagementSystemAPI/TaskManagementSystemAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BusinessLayer.Interfaces;
+using BusinessLayer.Mapping;
+using BusinessLayer.Services;
+using DataLayer;
+using DataLayer.Interfaces;
+using DataLayer.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskManagementSystemAPI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAppDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IAccountService, AccountService>();
+            
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            services.AddScoped<ITaskservice, TaskService>();
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
