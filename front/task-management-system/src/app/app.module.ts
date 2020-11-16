15a735c14b7247c4c0ff83834343966f8be042ed
Elsevar Rajabli <eradjabli7@gmail.com>
2020-11-16 13:19:27 +0200

    Update app.module.ts

--- front/task-management-system/src/app/app.module.ts
+++ front/task-management-system/src/app/app.module.ts
@@ -73,7 +73,7 @@ export function tokenGetter() {
     ForgotPasswordComponent,
     ResetPasswordComponent,
     PasswordChangeComponent,
-    UserUpdateComponent
+    UserUpdateComponent,
     TaskMenuComponent,
     StatusMenuComponent
   ],
