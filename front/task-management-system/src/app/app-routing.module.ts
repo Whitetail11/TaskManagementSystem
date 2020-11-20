7def5edce71cbe2d4ada7fcdd1ddf38fcc3b3b65
Elsevar Rajabli <eradjabli7@gmail.com>
2020-11-20 15:12:11 +0200

    added role guard; added using role guard for users page(allowed only for administrator)

--- front/task-management-system/src/app/app-routing.module.ts
+++ front/task-management-system/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { AuthenticatedUserService as AuthenticatedUser } from './services/authenticated-user.service'
 import { UnauthenticatedUserService as UnauthenticatedUser } from './services/unauthenticated-user.service'
+import { RoleUserService as RoleUser } from './services/role-user.service'
 import { UsersComponent } from './components/users/users.component';
 import { TaskComponent } from './components/task/task.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -12,6 +13,8 @@ import { UserComponent } from './components/user/user.component';
 import { ConfirmEmailComponent } from './components/confirm-email/confirm-email.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { from } from 'rxjs';
+import { AppConstants } from './models/appConstants';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -20,7 +23,14 @@ const appRoutes: Routes = [
   { path: 'tasks', component: TasksComponent, canActivate: [AuthenticatedUser] },
   { path: 'tasks/:id', component: TaskComponent, canActivate: [AuthenticatedUser] },
   { path: 'profile/:id', component: UserComponent, canActivate: [AuthenticatedUser] },
-  { path: 'users', component: UsersComponent, canActivate: [AuthenticatedUser] },
+  { 
+    path: 'users', 
+    component: UsersComponent, 
+    canActivate: [AuthenticatedUser, RoleUser], 
+    data: {
+      expectedRole: AppConstants.ADMIN_ROLE_NAME
+    }
+  },
   { path: 'users/:id', component: UserComponent, canActivate: [AuthenticatedUser] },
   { path: 'confirm-email', component: ConfirmEmailComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
