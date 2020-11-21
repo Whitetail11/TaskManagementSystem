1f67194dd323eacdc23ce1289e73fbd8b703ccb2
Elsevar Rajabli <eradjabli7@gmail.com>
2020-11-21 10:18:19 +0200

    Update environment.prod.ts

--- front/task-management-system/src/environments/environment.prod.ts
+++ front/task-management-system/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://dotnetdeveloper1-001-site1.etempurl.com/api/",
-  tokenAllowedDomains: ["dotnetdeveloper1-001-site1.etempurl.com"]
+  apiUrl: "https://taskmanagementsystemapi.azurewebsites.net/api/",
+  tokenAllowedDomains: ["taskmanagementsystemapi.azurewebsites.net"]
 };
